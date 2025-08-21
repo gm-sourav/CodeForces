016552bfe93fa9e5f1356e306d8646475915ed64
saklayen-dev <saklayen@media365.com.bd>
2025-08-21 10:33:09 +0600

    New

---
+++ Palindrome_Array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int A[n];
+    int B[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+        B[i] = A[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = B[n - 1 - i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] != B[i])
+        {
+            cout << "NO";
+        }
+    }
+
+    cout << " YES" << endl;
+}
