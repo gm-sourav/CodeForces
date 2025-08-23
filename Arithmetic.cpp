b01c0a7c0a9b3afb740fb4e42457387593536463
saklayen-dev <saklayen@media365.com.bd>
2025-08-24 00:03:46 +0600

    First Commit

---
+++ Arithmetic.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; 
+    cin >> t; 
+    while (t--) {
+        int n;
+        cin >> n;
+        long long sum = 0;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            sum += x;
+        }
+
+        if (sum == n) {
+            cout << 0 << "\n";
+        } else if (sum < n) {
+            cout << 1 << "\n";
+        } else { 
+            cout << sum - n << "\n";
+        }
+    }
+    return 0;
+}
