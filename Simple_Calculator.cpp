6b6d8aeb68a345c01807c8c0ba966feb17d8aa78
gm-sourav <souravmahbub13@gmail.com>
2025-08-30 01:30:37 +0600

    Second

--- Simple_Calculator.cpp
+++ Simple_Calculator.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main()
 {
-    int a,b;
+    long long a, b;
     cin >> a >> b;
+    long long sum = a + b;
+    long long multi = a * b;
+    long long sub = a - b;
 
-    
+    cout << a << " + " << b << " = " << sum << endl;
+    cout << a << " * " << b << " = " << multi << endl;
+    cout << a << " - " << b << " = " << sub << endl;
 
-
-    
     return 0;
 }
