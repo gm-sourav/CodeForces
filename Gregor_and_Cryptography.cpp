7e7286228aa9654f1ab3595394716f2c86a1653a
gm-sourav <souravmahbub13@gmail.com>
2025-09-03 23:41:10 +0600

    Second

---
+++ Gregor_and_Cryptography.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+
+        long long a = 2;
+        long long b = n - 1;
+
+        cout << a << " " << b << endl;
+    }
+    
+
+
+    
+    return 0;
+}
