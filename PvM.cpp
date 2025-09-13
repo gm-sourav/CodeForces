8b1b3a79021325ae13fc06ca16c41df3734bb2e9
gm-sourav <souravmahbub13@gmail.com>
2025-09-13 21:43:59 +0600

    Second

---
+++ PvM.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a, b;
+        cin >> a >> b;
+        cout << min({a, b, (a + b) / 4}) << "\n";
+    }
+    return 0;
+}
