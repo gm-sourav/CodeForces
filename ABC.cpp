eb3c4491fd0c46523e6c559262d62f2791699753
gm-sourav <souravmahbub13@gmail.com>
2025-09-18 23:36:36 +0600

    Second

---
+++ ABC.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        if (n == 1) {
+            cout << "YES\n";
+        } else if (n == 2) {
+            if (s[0] != s[1]) cout << "YES\n";
+            else cout << "NO\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
