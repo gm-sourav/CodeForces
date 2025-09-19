7d5dbeeaa50b0d3cfa312392ee07243ce844e817
gm-sourav <souravmahbub13@gmail.com>
2025-09-19 23:25:01 +0600

    Second

---
+++ Palindrome.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    string s2 = s;
+    reverse(s.begin(), s.end());
+    if (s == s2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
