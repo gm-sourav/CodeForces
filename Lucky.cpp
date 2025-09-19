a7ec26022f3e3051644f82faf04dc478de4f7a84
gm-sourav <souravmahbub13@gmail.com>
2025-09-20 00:14:29 +0600

    Second

--- Lucky.cpp
+++ Lucky.cpp
@@ -2,14 +2,45 @@
 using namespace std;
 int main()
 {
-    int a[6];
-    for (int i = 0; i < count; i++)
+
+    int t;
+    cin >> t;
+    while (t--)
     {
-        /* code */
+        string s;
+        cin >> s;
+        int sum1 = 0;
+        int sum2 = 0;
+        // for (int i = 0; i < 3; i++)
+        // {
+        //     sum1 += s[i];
+        // }
+        // for (int i = 3; i < 6; i++)
+        // {
+        //     sum2 += s[i];
+        // }
+        for (int i = 0; i < 6; i++)
+        {
+            if (i < 3)
+            {
+                sum1 += s[i];
+            } else {
+                sum2 += s[i];
+            }
+            
+        }
+
+        if (sum1 == sum2)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+
+        
+        
     }
-    
-
-
-    
     return 0;
 }
