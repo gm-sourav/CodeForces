bc36d3520e5b35a289760469f919d1e1e329fb25
gm-sourav <souravmahbub13@gmail.com>
2025-09-23 23:12:03 +0600

    Second

---
+++ prt.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n % 2 == 0) {
+            for (int i = 1; i <= n; i += 2) {
+                cout << i + 1 << " " << i << " ";
+            }
+        } else {
+            
+            for (int i = 1; i <= n - 3; i += 2) {
+                cout << i + 1 << " " << i << " ";
+            }
+            cout << n - 1 << " " << n << " " << n - 2;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
