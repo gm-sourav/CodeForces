f74c07d5d360c5a828e396861859c811f7723e1f
gm-sourav <souravmahbub13@gmail.com>
2025-10-08 22:08:33 +0600

    Second

---
+++ A_Domino_Disaster.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'U') ans += 'D';
+            else if (s[i] == 'D') ans += 'U';
+            else ans += s[i]; // 
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
